39aafcd78b947f24e5f0ff8453c2826e3e394a33
Grauly <nils.dellemann@web.de>
2024-11-18 23:28:19 +0100

    feat: plain item register helper

--- src/main/kotlin/grauly/ritualis/ModItems.kt
+++ src/main/kotlin/grauly/ritualis/ModItems.kt
@@ -4,18 +4,30 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.block.Block
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.item.Item.Settings
 import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
+import kotlin.reflect.KFunction1
 
 object ModItems {
 
+    val DUMMY_ITEM = registerPlainItem("dummy", ::Item)
+
+    private fun registerPlainItem(path: String, constructor: KFunction1<Settings, Item>, settings: Settings = Settings()): Item {
+        val id = Identifier.of(Ritualis.MODID, path)
+        val key = RegistryKey.of(RegistryKeys.ITEM, id)
+        settings.registryKey(key).useItemPrefixedTranslationKey()
+        val item = constructor.call(settings)
+        return Registry.register(Registries.ITEM, key, item)
+    }
+
     fun registerCandleBlockItem(candleBlock: Block, id: Identifier) {
         val key = RegistryKey.of(RegistryKeys.ITEM, id)
-        val settings = Item.Settings()
+        val settings = Settings()
         settings.registryKey(key).useBlockPrefixedTranslationKey()
         register(id, BlockItem(candleBlock, settings))
     }
@@ -23,6 +35,7 @@ object ModItems {
     fun init() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register {group ->
             ModBlocks.candles.forEach {candle -> group.add(candle.asItem())}
+            group.add(DUMMY_ITEM)
         }
     }
 
@@ -30,8 +43,9 @@ object ModItems {
         register(Identifier.of(Ritualis.MODID, id), item)
 
     private fun register(id: Identifier, item: Item): Item =
-        Registry.register(Registries.ITEM, id, item)
+        register(RegistryKey.of(RegistryKeys.ITEM, id), item)
 
-    private fun register(key: RegistryKey<Item>, item: Item): Item =
-        Registry.register(Registries.ITEM, key, item)
+    private fun register(key: RegistryKey<Item>, item: Item): Item {
+        return Registry.register(Registries.ITEM, key, item)
+    }
 }
