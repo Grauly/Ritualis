7370b02e4de45f0bba9a5ba09c0cde88977b6e1c
Grauly <nils.dellemann@web.de>
2024-11-29 19:53:16 +0100

    refactor: extinguish particle to full custom class

--- src/main/kotlin/grauly/ritualis/particle/ExtinguishParticleEffect.kt
+++ src/main/kotlin/grauly/ritualis/particle/ExtinguishParticleEffect.kt
@@ -1,38 +1,32 @@
 package grauly.ritualis.particle
 
 import com.mojang.serialization.Codec
-import com.mojang.serialization.DataResult
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
+import grauly.ritualis.ModParticles
 import net.minecraft.network.RegistryByteBuf
 import net.minecraft.network.codec.PacketCodec
 import net.minecraft.network.codec.PacketCodecs
-import net.minecraft.particle.VibrationParticleEffect
-import net.minecraft.world.event.EntityPositionSource
-import net.minecraft.world.event.PositionSource
+import net.minecraft.particle.ParticleEffect
+import net.minecraft.particle.ParticleType
+import net.minecraft.util.math.Vec3d
 
-class ExtinguishParticleEffect(destination: PositionSource?, arrivalInTicks: Int) : VibrationParticleEffect(
-    destination,
-    arrivalInTicks
-) {
+class ExtinguishParticleEffect(val target: Vec3d, val arrivalInTicks: Int) : ParticleEffect {
+    override fun getType(): ParticleType<*> = ModParticles.EXTINGUISH
+
     companion object {
-        private val POSITION_SOURCE_CODEC: Codec<PositionSource> = PositionSource.CODEC
-            .validate { positionSource: PositionSource ->
-                if (positionSource is EntityPositionSource) DataResult.error { "Entity position sources are not allowed" }
-                else DataResult.success(positionSource)
-            }
         val CODEC: MapCodec<ExtinguishParticleEffect> =
             RecordCodecBuilder.mapCodec { instance: RecordCodecBuilder.Instance<ExtinguishParticleEffect> ->
                 instance.group(
-                    POSITION_SOURCE_CODEC.fieldOf("destination").forGetter(ExtinguishParticleEffect::getVibration),
-                    Codec.INT.fieldOf("arrival_in_ticks").forGetter(ExtinguishParticleEffect::getArrivalInTicks)
+                    Vec3d.CODEC.fieldOf("target").forGetter(ExtinguishParticleEffect::target),
+                    Codec.INT.fieldOf("arrival_in_ticks").forGetter(ExtinguishParticleEffect::arrivalInTicks)
                 ).apply(instance, ::ExtinguishParticleEffect)
             }
         val PACKET_CODEC: PacketCodec<RegistryByteBuf, ExtinguishParticleEffect> = PacketCodec.tuple(
-            PositionSource.PACKET_CODEC,
-            ExtinguishParticleEffect::getVibration,
+            Vec3d.PACKET_CODEC,
+            ExtinguishParticleEffect::target,
             PacketCodecs.VAR_INT,
-            ExtinguishParticleEffect::getArrivalInTicks,
+            ExtinguishParticleEffect::arrivalInTicks,
             ::ExtinguishParticleEffect
         )
     }
