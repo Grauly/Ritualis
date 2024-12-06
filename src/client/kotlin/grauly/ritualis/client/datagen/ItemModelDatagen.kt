3bde8f67c2018670c306453b398d66007c5fd7b9
Grauly <nils.dellemann@web.de>
2024-12-06 18:24:04 +0100

    refactor: remove redundant mixin

--- src/client/kotlin/grauly/ritualis/client/datagen/ItemModelDatagen.kt
+++ src/client/kotlin/grauly/ritualis/client/datagen/ItemModelDatagen.kt
@@ -2,7 +2,6 @@ package grauly.ritualis.client.datagen
 
 import grauly.ritualis.ModBlocks
 import grauly.ritualis.Ritualis
-import grauly.ritualis.mixin.client.ItemModelGeneratorAccessor
 import net.minecraft.client.data.ItemModelGenerator
 import net.minecraft.client.data.ItemModels
 import net.minecraft.item.Item
@@ -11,7 +10,6 @@ import net.minecraft.util.Identifier
 object ItemModelDatagen {
 
     fun generateItemModels(itemModelGenerator: ItemModelGenerator) {
-        itemModelGenerator as ItemModelGeneratorAccessor
         generateCandleItemModel(ModBlocks.candles[0].asItem(), "", itemModelGenerator)
         for (index: Int in 1..Ritualis.COLOR_ORDER.size) {
             val color = Ritualis.COLOR_ORDER[index - 1]
@@ -24,7 +22,7 @@ object ItemModelDatagen {
     private fun generateCandleItemModel(
         item: Item,
         colorString: String,
-        itemModelGeneratorAccessor: ItemModelGeneratorAccessor
+        itemModelGeneratorAccessor: ItemModelGenerator
     ) {
         val baseCandle = ItemModels.basic(Identifier.of("minecraft", "item/${colorString}candle"))
         val candleOverlay = ItemModels.basic(Identifier.of(Ritualis.MODID, "item/ritual_candle_overlay"))
