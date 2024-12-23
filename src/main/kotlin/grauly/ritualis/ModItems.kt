065c7a6de4ae184451624ce1ff157bda493f18c9
Grauly <nils.dellemann@web.de>
2024-12-23 16:06:12 +0100

    feat: block item

--- src/main/kotlin/grauly/ritualis/ModItems.kt
+++ src/main/kotlin/grauly/ritualis/ModItems.kt
@@ -6,6 +6,7 @@ import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.Item.Settings
 import net.minecraft.item.ItemGroups
+import net.minecraft.item.Items
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
@@ -14,6 +15,15 @@ import net.minecraft.util.Identifier
 import kotlin.reflect.KFunction1
 
 object ModItems {
+
+    val FLOATING_BOOK_ITEM = registerBlockItem(ModBlocks.FLOATING_BOOK, "floating_book")
+
+    private fun registerBlockItem(block: Block, itemID: String, settings: Settings = Settings()): Item {
+        val id = Identifier.of(Ritualis.MODID, itemID)
+        val key = RegistryKey.of(RegistryKeys.ITEM, id)
+        settings.registryKey(key).useBlockPrefixedTranslationKey()
+        return Registry.register(Registries.ITEM, key, BlockItem(block, settings))
+    }
 
     private fun registerPlainItem(
         path: String,
@@ -37,6 +47,9 @@ object ModItems {
     fun init() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register { group ->
             ModBlocks.candles.forEach { candle -> group.add(candle.asItem()) }
+        }
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register { group ->
+            group.addAfter(Items.ENCHANTING_TABLE, FLOATING_BOOK_ITEM)
         }
     }
 
