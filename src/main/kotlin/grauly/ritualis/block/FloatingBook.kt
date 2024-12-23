bb475862514df6da272816ca3bc42ba50d9b5c0c
Grauly <nils.dellemann@web.de>
2024-12-23 16:34:37 +0100

    feat: configure as much as possible

--- src/main/kotlin/grauly/ritualis/block/FloatingBook.kt
+++ src/main/kotlin/grauly/ritualis/block/FloatingBook.kt
@@ -1,13 +1,17 @@
 package grauly.ritualis.block
 
 import com.mojang.serialization.MapCodec
+import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
 import net.minecraft.block.BlockWithEntity
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.math.BlockPos
 
-class FloatingBook(settings: Settings) : BlockWithEntity(settings) {
+class FloatingBook(settings: Settings) : BlockWithEntity(settings.noCollision().breakInstantly().noBlockBreakParticles().nonOpaque()) {
     override fun getCodec(): MapCodec<out BlockWithEntity> = createCodec(::FloatingBook)
 
     override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity = FloatingBookBlockEntity(pos, state)
+
+    override fun getRenderType(state: BlockState?): BlockRenderType = BlockRenderType.INVISIBLE
+
 }
