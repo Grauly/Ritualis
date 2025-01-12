e54fe48418479d4a13164325ae6677aede5f2a30
Grauly <nils.dellemann@web.de>
2025-01-12 02:21:19 +0100

    feat: finally, the book looks at me.

--- src/client/kotlin/grauly/ritualis/client/block/FloatingBlockEntityRenderer.kt
+++ src/client/kotlin/grauly/ritualis/client/block/FloatingBlockEntityRenderer.kt
@@ -1,16 +1,32 @@
 package grauly.ritualis.client.block
 
+import com.mojang.blaze3d.systems.RenderSystem
 import grauly.ritualis.Ritualis
 import grauly.ritualis.block.FloatingBookBlockEntity
+import grauly.ritualis.extensions.toSpherical
+import net.minecraft.block.DirtPathBlock
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.data.Models
+import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.RenderLayers
 import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.VertexRendering
 import net.minecraft.client.render.block.entity.BlockEntityRenderer
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory
 import net.minecraft.client.render.block.entity.EnchantingTableBlockEntityRenderer
+import net.minecraft.client.render.debug.DebugRenderer
 import net.minecraft.client.render.entity.model.BookModel
 import net.minecraft.client.render.entity.model.EntityModelLayers
+import net.minecraft.client.render.item.ItemRenderState
+import net.minecraft.client.render.item.ItemRenderer
 import net.minecraft.client.util.math.MatrixStack
-import kotlin.math.sin
+import net.minecraft.item.ModelTransformationMode
+import net.minecraft.util.math.RotationAxis
+import net.minecraft.util.math.Vec3d
+import org.joml.Quaternionf
+import org.joml.Vector3f
+import kotlin.math.*
 
 class FloatingBlockEntityRenderer(ctx: BlockEntityRendererFactory.Context) :
     BlockEntityRenderer<FloatingBookBlockEntity> {
@@ -31,14 +47,126 @@ class FloatingBlockEntityRenderer(ctx: BlockEntityRendererFactory.Context) :
             vertexConsumers,
             RenderLayer::getEntitySolid
         )
+        val context = entity.renderingContext
+        val time = context.ticks + tickDelta
+        val axis = context.extractAxisFromQuaternion(context.currentRotation)
         matrices.push()
-        matrices.translate(.5,sin(entity.ticks*.1)*.5 + .5f,.5)
+        debugSpace(matrices, vertexConsumers)
+
+/*
+        val up = Vec3d(.0,1.0,.0)
+        val dot = up.dotProduct(context.lookAtVector)
+        val cross = up.crossProduct(context.lookAtVector)
+        val w = context.lookAtVector.length() + dot
+        val quat = Quaternionf(cross.x, cross.y, cross.z, w).normalize()
+*/
+/*
+        val q2 = quaternionAroundAxisAngle(Vec3d(.0,1.0,.0),90f)
+        quat.mul(q2)
+*/
+        val spherical = context.lookAtVector.toSpherical()
+        val q1 = quaternionAroundAxisAngle(Vec3d(.0,1.0,.0), -Math.toDegrees(spherical.z).toFloat())
+        val q2 = quaternionAroundAxisAngle(Vec3d(.0,.0,1.0), -Math.toDegrees(spherical.y).toFloat())
+        val q3 = quaternionAroundAxisAngle(Vec3d(.0,.0,1.0), 90f)
+        val quat = q1.mul(q2).mul(q3)
+
+        visualizeQuaternion(matrices, vertexConsumers, quat, 0xFFFFFFFF.toInt())
+        matrices.translate(.5, .5, .5)
+        //matrices.translate(.5,sin(time*.1)*.2 + .5f,.5)
+        matrices.multiply(quat)
+        debugSpace(matrices,vertexConsumers)
+
         //pageTurnAmount: ???
         //leftFlipAmount: left page flip amount from 0 -> 1
         //rightFlipAmount: right page flip amount from 0 -> 1
         //pageTurnSpeed: open/close of book from 0 -> 1
-        book.setPageAngles(0f,.5f,0f,1f)
+        book.setPageAngles(0f, .5f, 0f, 1f)
         book.render(matrices, vertexConsumer, light, overlay)
         matrices.pop()
     }
+
+    fun visualizeQuaternion(
+        matrices: MatrixStack,
+        vertexConsumers: VertexConsumerProvider,
+        quaternion: Quaternionf,
+        color: Int = (0xFFFF0000.toInt())
+    ) {
+        val angle = acos(quaternion.w).toDouble()
+        val quaternionAxis =
+            Vec3d(quaternion.x.toDouble(), quaternion.y.toDouble(), quaternion.z.toDouble()).multiply(1 / sin(angle))
+        VertexRendering.drawVector(
+            matrices,
+            vertexConsumers.getBuffer(RenderLayer.LINES),
+            Vector3f(.5f, .5f, .5f),
+            quaternionAxis,
+            color
+        )
+        val textPoint = Vec3d(.5, .5, .5).add(quaternionAxis.multiply(1.2))
+
+        putDebugText(
+        "${Math.toDegrees(angle * 2)}",
+            textPoint,
+            matrices,
+            vertexConsumers
+        )
+    }
+
+    fun putDebugText(text: String, offset: Vec3d, matrices: MatrixStack, vertexConsumers: VertexConsumerProvider, color: Int = 0xFFFFFFFF.toInt()) {
+        matrices.push()
+        matrices.translate(offset)
+        matrices.multiply(MinecraftClient.getInstance().gameRenderer.camera.rotation)
+        matrices.scale(0.02f, -0.02f, 0.02f)
+        MinecraftClient.getInstance().textRenderer.draw(
+            text,
+            .0f,
+            .0f,
+            color,
+            false,
+            matrices.peek().positionMatrix,
+            vertexConsumers,
+            TextRenderer.TextLayerType.SEE_THROUGH,
+            0,
+            15728880
+        )
+        matrices.pop()
+    }
+
+    fun debugSpace(matrices: MatrixStack, vertexConsumers: VertexConsumerProvider) {
+        VertexRendering.drawVector(
+            matrices,
+            vertexConsumers.getBuffer(RenderLayer.LINES),
+            Vector3f(.0f, .0f, .0f),
+            Vec3d(1.0, .0, .0),
+            0xFFFF0000.toInt()
+        )
+        VertexRendering.drawVector(
+            matrices,
+            vertexConsumers.getBuffer(RenderLayer.LINES),
+            Vector3f(.0f, .0f, .0f),
+            Vec3d(.0, 1.0, .0),
+            0xFF00FF00.toInt()
+        )
+        VertexRendering.drawVector(
+            matrices,
+            vertexConsumers.getBuffer(RenderLayer.LINES),
+            Vector3f(.0f, .0f, .0f),
+            Vec3d(.0, .0, 1.0),
+            0xFF0000FF.toInt()
+        )
+    }
+
+    fun quaternionAroundAxisAngle(axis: Vec3d, angle: Float): Quaternionf {
+        var radiansAngle = Math.toRadians(angle.toDouble()) / 2
+        /*
+                while(radiansAngle > 2* PI) radiansAngle -= PI
+                while(radiansAngle < 0) radiansAngle += PI
+        */
+        val scaledAxis = axis.normalize().multiply(sin(radiansAngle)).toVector3f()
+        return Quaternionf(scaledAxis.x, scaledAxis.y, scaledAxis.z, cos(radiansAngle).toFloat())
+    }
+
+    /*
+        fun calculateRotationQuaternion(oldFacing: Vec3d, newFacing: Vec3d, tickDelta: Float): Quaternionf {
+        }
+    */
 }
--- src/main/kotlin/grauly/ritualis/block/FloatingBookBlockEntity.kt
+++ src/main/kotlin/grauly/ritualis/block/FloatingBookBlockEntity.kt
@@ -3,8 +3,13 @@ package grauly.ritualis.block
 import grauly.ritualis.ModBlockEntities
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
+import org.joml.Quaternionf
+import kotlin.math.*
 
 class FloatingBookBlockEntity(
     pos: BlockPos,
@@ -14,8 +19,65 @@ class FloatingBookBlockEntity(
     pos,
     state
 ) {
-    var ticks = 0
+    //only needed client side
+    var renderingContext: RenderingContext = RenderingContext(0, pos.toCenterPos(), pos.toCenterPos())
+
     fun tick(world: World, pos: BlockPos, state: BlockState) {
-        ticks++
+        renderingTick(world, pos, state)
+    }
+
+    fun renderingTick(world: World, pos: BlockPos, state: BlockState) {
+        renderingContext.ticks++
+        val searchBox = Box(pos).expand(5.0)
+        val lookAtPlayerTarget: PlayerEntity? = world.getNonSpectatingEntities(PlayerEntity::class.java, searchBox).reduceOrNull { currentClosest: PlayerEntity, playerEntity: PlayerEntity ->
+            val currentDistance = pos.toCenterPos().squaredDistanceTo(currentClosest.eyePos)
+            val newDistance = pos.toCenterPos().squaredDistanceTo(playerEntity.eyePos)
+            return@reduceOrNull if(currentDistance > newDistance) playerEntity else currentClosest
+        }
+        //update look at target
+        if(lookAtPlayerTarget != null) {
+            renderingContext.ticksSincePlayerLookAt = 0
+            renderingContext.updateLookAtTarget(lookAtPlayerTarget.eyePos, pos.toCenterPos())
+        } else {
+            renderingContext.ticksSincePlayerLookAt++
+        }
+    }
+
+    data class RenderingContext(
+        var ticks: Int,
+        var lastLookAtTarget: Vec3d,
+        var lookAtTarget: Vec3d,
+        var lookAtVector: Vec3d = Vec3d(.0,.0,.0),
+        var lastRotation: Quaternionf = Quaternionf(),
+        var currentRotation: Quaternionf = Quaternionf(),
+        var ticksSincePlayerLookAt: Int = 0,
+        var ticksUntilNewTarget: Int = 0,
+        var bookOpenState: Float = 1f
+    ) {
+        fun updateLookAtTarget(newTarget: Vec3d, currentLocation: Vec3d) {
+            if (newTarget == lookAtTarget) return
+            lastLookAtTarget = lookAtTarget
+            lookAtTarget = newTarget
+            lookAtVector = lookAtTarget.subtract(currentLocation)
+            val direction = lookAtVector.normalize()
+            val origin = Vec3d(1.0,0.0,0.0)
+            var localUp = origin.crossProduct(direction)
+            //if(localUp.y < 0) localUp = localUp.multiply(-1.0)
+            val angle = Math.toDegrees(origin.dotProduct(direction))
+            currentRotation = quaternionAroundAxisAngle(localUp, abs(angle.toFloat()) + 90f)
+        }
+
+        fun quaternionAroundAxisAngle(axis: Vec3d, angle: Float): Quaternionf {
+            var radiansAngle = Math.toRadians(angle.toDouble())/2
+            while(radiansAngle > 2*PI) radiansAngle -= PI
+            while(radiansAngle < 0) radiansAngle += PI
+            val scaledAxis = axis.normalize().multiply(sin(radiansAngle)).toVector3f()
+            return Quaternionf(scaledAxis.x, scaledAxis.y, scaledAxis.z, sin(radiansAngle + PI/2).toFloat())
+        }
+
+        fun extractAxisFromQuaternion(quaternionf: Quaternionf): Vec3d {
+            val angle = cosh(quaternionf.w)
+            return Vec3d(quaternionf.x.toDouble(), quaternionf.y.toDouble(), quaternionf.z.toDouble()).multiply(sin(angle).toDouble())
+        }
     }
 }
