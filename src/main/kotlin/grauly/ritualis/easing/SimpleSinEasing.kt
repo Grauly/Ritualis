134306bb703dc032fcbf5132db2bb668634d291f
Grauly <nils.dellemann@web.de>
2025-02-12 00:18:25 +0100

    fix: movement should no longer NaN

--- src/main/kotlin/grauly/ritualis/easing/SimpleSinEasing.kt
+++ src/main/kotlin/grauly/ritualis/easing/SimpleSinEasing.kt
@@ -5,7 +5,7 @@ import kotlin.math.cos
 class SimpleSinEasing: EasingHandler {
     override fun retrieveOffset(delta: Double, realDelta: Double): Double = easeInOutSine(delta)
 
-    override fun recalculateDelta(oldDelta: Double, immediateRealDelta: Double, oldRealDelta: Double): Double = oldDelta
+    override fun recalculateDelta(oldDelta: Double, immediateRealDelta: Double, oldRealDelta: Double): Double = 0.0
 
     private fun easeInOutSine(x: Double): Double{
         return (-(cos(Math.PI * x) - 1) / 2);
--- src/main/kotlin/grauly/ritualis/util/PositionHandler.kt
+++ src/main/kotlin/grauly/ritualis/util/PositionHandler.kt
@@ -1,25 +1,28 @@
 package grauly.ritualis.util
 
+import grauly.ritualis.Ritualis
 import grauly.ritualis.easing.EasingHandler
 import net.minecraft.util.math.Vec3d
+import kotlin.math.min
 import kotlin.math.pow
 
 class PositionHandler(
-    private val maxMovementPerTick: Double = 0.1,
+    private val maxMovementPerTick: Double = 0.01,
     private val epsilon: Double = 0.001,
     private val easing: EasingHandler = EasingHandler.IdentityEasingHandler(),
     private var startPosition: Vec3d = Vec3d(.0, .0, .0)
 ) {
     private var currentPosition: Vec3d = Vec3d(.0, .0, .0)
-    private var targetPosition: Vec3d = Vec3d(.0, .0, .0)
+    private var targetPosition: Vec3d = startPosition
     private var delta: Double = 0.0
 
     fun partialTick(deltaTime: Double) {
         if (currentPosition.squaredDistanceTo(targetPosition) <= epsilon.pow(2.0)) return
         val fullMovementVector = targetPosition.subtract(startPosition)
         val fullLength = fullMovementVector.length()
+        if (fullLength == 0.0) return
         val deltaOffset = 1 / (fullLength / maxMovementPerTick * deltaTime)
-        delta += deltaOffset
+        delta = min(1.0, delta + deltaOffset)
         currentPosition = startPosition.lerp(targetPosition, delta)
     }
 
@@ -28,7 +31,12 @@ class PositionHandler(
         val newDistance = newMovement.length()
         val oldDistanceTraveled = startPosition.lerp(targetPosition, delta).length()
         val newDelta = easing.recalculateDelta(delta, newDistance, oldDistanceTraveled)
-        startPosition = newMovement.multiply(-1/newDelta)
+        startPosition = newPosition.add(
+            if (newDelta == 0.0)
+                newMovement.multiply(-1.0)
+            else
+                newMovement.multiply(-1 / newDelta)
+        )
         targetPosition = newPosition
         delta = newDelta
     }
@@ -41,5 +49,8 @@ class PositionHandler(
     }
 
     //yes, this does mean that it CAN move faster then maxMovementPerTick, but as long as it still arrives on time...
-    fun getCurrentPosition(): Vec3d = startPosition.lerp(targetPosition, easing.retrieveOffset(delta, startPosition.lerp(targetPosition, delta).length()))
+    fun getCurrentPosition(): Vec3d = startPosition.lerp(
+        targetPosition,
+        easing.retrieveOffset(delta, startPosition.lerp(targetPosition, delta).length())
+    )
 }
