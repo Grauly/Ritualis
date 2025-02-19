c9e7de8c76e6c8bb00f8e212186fd80c5d1f38f0
Grauly <nils.dellemann@web.de>
2025-02-19 05:44:51 +0100

    feat: first working rendering

--- src/client/kotlin/grauly/ritualis/client/datagen/BlockModelDatagen.kt
+++ src/client/kotlin/grauly/ritualis/client/datagen/BlockModelDatagen.kt
@@ -2,6 +2,7 @@ package grauly.ritualis.client.datagen
 
 import grauly.ritualis.ModBlocks
 import grauly.ritualis.Ritualis
+import grauly.ritualis.block.RitualLine
 import net.minecraft.block.Block
 import net.minecraft.client.data.*
 import net.minecraft.state.property.Properties
@@ -22,9 +23,26 @@ object BlockModelDatagen {
             )
         }
         blockStateModelGenerator.registerGeneric(ModBlocks.FLOATING_BOOK)
+        val lineSegment = "block/ritual_line"
+        val lineNotch = "block/ritual_notch"
+        blockStateModelGenerator.blockStateCollector.accept(
+            MultipartBlockStateSupplier.create(ModBlocks.RITUAL_LINE)
+                .with(BlockStateVariant.create().put(VariantSettings.MODEL, Identifier.of(Ritualis.MODID, "block/ritual_dot")))
+                .with(When.create().set(RitualLine.CONNECTED_NORTH, true), createRotationSegmentBSV(VariantSettings.Rotation.R0, lineSegment))
+                .with(When.create().set(RitualLine.CONNECTED_EAST, true), createRotationSegmentBSV(VariantSettings.Rotation.R90, lineSegment))
+                .with(When.create().set(RitualLine.CONNECTED_SOUTH, true), createRotationSegmentBSV(VariantSettings.Rotation.R180, lineSegment))
+                .with(When.create().set(RitualLine.CONNECTED_WEST, true), createRotationSegmentBSV(VariantSettings.Rotation.R270, lineSegment))
+                .with(When.create().set(RitualLine.CONNECTED_NORTH, true).set(RitualLine.CONNECTED_EAST, true), createRotationSegmentBSV(VariantSettings.Rotation.R0, lineNotch))
+                .with(When.create().set(RitualLine.CONNECTED_EAST, true).set(RitualLine.CONNECTED_SOUTH, true), createRotationSegmentBSV(VariantSettings.Rotation.R90, lineNotch))
+                .with(When.create().set(RitualLine.CONNECTED_SOUTH, true).set(RitualLine.CONNECTED_WEST, true), createRotationSegmentBSV(VariantSettings.Rotation.R180, lineNotch))
+                .with(When.create().set(RitualLine.CONNECTED_WEST, true).set(RitualLine.CONNECTED_NORTH, true), createRotationSegmentBSV(VariantSettings.Rotation.R270, lineNotch))
+        )
     }
 
-    private fun createBlockStateVariant(candleNumber: Int, lit: Boolean, colorPrefix: String): BlockStateVariant? {
+    private fun createRotationSegmentBSV(rotation: VariantSettings.Rotation, model: String): BlockStateVariant =
+        BlockStateVariant.create().put(VariantSettings.Y, rotation).put(VariantSettings.MODEL, Identifier.of(Ritualis.MODID, model))
+
+    private fun createCandleBSV(candleNumber: Int, lit: Boolean, colorPrefix: String): BlockStateVariant? {
         val candleSuffix = StringBuilder("candle")
         if (candleNumber > 1) candleSuffix.append("s")
         val modelPath = StringBuilder("block/${colorPrefix}candle_${numbers[candleNumber - 1]}_${candleSuffix}")
@@ -39,8 +57,8 @@ object BlockModelDatagen {
     ) {
         val variantMap = BlockStateVariantMap.create(Properties.CANDLES, Properties.LIT)
         for (candleNum: Int in 1..4) {
-            variantMap.register(candleNum, false, createBlockStateVariant(candleNum, false, colorPrefix))
-            variantMap.register(candleNum, true, createBlockStateVariant(candleNum, true, colorPrefix))
+            variantMap.register(candleNum, false, createCandleBSV(candleNum, false, colorPrefix))
+            variantMap.register(candleNum, true, createCandleBSV(candleNum, true, colorPrefix))
         }
         blockStateCollector.accept(VariantsBlockStateSupplier.create(candle).coordinate(variantMap))
     }
