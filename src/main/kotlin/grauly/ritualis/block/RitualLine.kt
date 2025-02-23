6b8f4c0697626b32f8cf22f84bc213566e9b12af
Grauly <nils.dellemann@web.de>
2025-02-23 01:52:17 +0100

    feat: outline shape

--- src/main/kotlin/grauly/ritualis/block/RitualLine.kt
+++ src/main/kotlin/grauly/ritualis/block/RitualLine.kt
@@ -1,10 +1,12 @@
 package grauly.ritualis.block
 
+import com.google.common.collect.ImmutableMap
 import grauly.ritualis.ModBlockTags
 import grauly.ritualis.Ritualis
 import grauly.ritualis.util.OkLabColorSpace
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.ShapeContext
 import net.minecraft.item.ItemPlacementContext
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.BooleanProperty
@@ -12,9 +14,13 @@ import net.minecraft.state.property.IntProperty
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.random.Random
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.BlockView
 import net.minecraft.world.WorldView
 import net.minecraft.world.tick.ScheduledTickView
 import java.awt.Color
+import java.util.*
 
 class RitualLine(settings: Settings) : Block(settings) {
 
@@ -25,6 +31,41 @@ class RitualLine(settings: Settings) : Block(settings) {
             .with(CONNECTED_SOUTH, false)
             .with(CONNECTED_WEST, false)
             .with(POWER, 0)
+        for (state in stateManager.states) {
+            if (state.get(POWER) != 0) continue
+            SHAPES[state] = getShapeForState(state)
+        }
+    }
+
+    private fun getShapeForState(state: BlockState): VoxelShape {
+        val neededShapes: MutableList<VoxelShape> = mutableListOf()
+        if (state.get(CONNECTED_NORTH)) neededShapes.add(CONNECTOR_SHAPES[Direction.NORTH]!!)
+        if (state.get(CONNECTED_EAST)) neededShapes.add(CONNECTOR_SHAPES[Direction.EAST]!!)
+        if (state.get(CONNECTED_SOUTH)) neededShapes.add(CONNECTOR_SHAPES[Direction.SOUTH]!!)
+        if (state.get(CONNECTED_WEST)) neededShapes.add(CONNECTOR_SHAPES[Direction.WEST]!!)
+        if (state.get(CONNECTED_NORTH) && state.get(CONNECTED_EAST))
+            neededShapes.add(createCuboidShape(11.0, 0.0, 3.0, 13.0, 1.0, 5.0))
+        if (state.get(CONNECTED_EAST) && state.get(CONNECTED_SOUTH))
+            neededShapes.add(createCuboidShape(11.0, 0.0, 11.0, 13.0, 1.0, 13.0))
+        if (state.get(CONNECTED_SOUTH) && state.get(CONNECTED_WEST))
+            neededShapes.add(createCuboidShape(3.0, 0.0, 11.0, 5.0, 1.0, 13.0))
+        if (state.get(CONNECTED_WEST) && state.get(CONNECTED_NORTH))
+            neededShapes.add(createCuboidShape(3.0, 0.0, 3.0, 5.0, 1.0, 5.0))
+        var finalVoxelShape = DOT_SHAPE
+        for (connectionShape in neededShapes) {
+            finalVoxelShape = VoxelShapes.union(finalVoxelShape, connectionShape)
+        }
+        return finalVoxelShape
+    }
+
+    override fun getOutlineShape(
+        state: BlockState,
+        world: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape {
+        val lookupState = state.with(POWER, 0)
+        return SHAPES[lookupState]!!
     }
 
     override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
@@ -75,6 +116,18 @@ class RitualLine(settings: Settings) : Block(settings) {
         private val START_COLOR: Color = Color(0x8b, 0x02, 0xd6) //0xff8b02d6.toInt()
         private val END_COLOR: Color = Color(0xcb, 0x02, 0xd6) //0xffcb02d6.toInt()
         val COLORS = generatePallete()
+
+        val DOT_SHAPE: VoxelShape = createCuboidShape(5.0, 0.0, 5.0, 11.0, 1.0, 11.0)
+        val CONNECTOR_SHAPES: EnumMap<Direction, VoxelShape> = EnumMap(
+            ImmutableMap.of(
+                Direction.NORTH, createCuboidShape(5.0, 0.0, 0.0, 11.0, 1.0, 5.0),
+                Direction.EAST, createCuboidShape(11.0, 0.0, 5.0, 16.0, 1.0, 11.0),
+                Direction.SOUTH, createCuboidShape(5.0, 0.0, 11.0, 11.0, 1.0, 16.0),
+                Direction.WEST, createCuboidShape(0.0, 0.0, 5.0, 5.0, 1.0, 11.0)
+            )
+        )
+
+        var SHAPES: HashMap<BlockState, VoxelShape> = HashMap()
 
         private fun generatePallete(): IntArray {
             val valueAmount = POWER.values.last()
